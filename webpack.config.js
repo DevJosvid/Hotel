0a8322dc37bb19123bfed00b83328a7efb57cd72
Josvid Calderon <Josvid.calderon@sinco.com.co>
2021-02-16 17:15:47 -0500

    webpack fixed

--- webpack.config.js
+++ webpack.config.js
@@ -13,49 +13,9 @@ module.exports = {
             },
             {
                 test: /\.css$/,
-                loader: "style-loader! css-loader"
+                use: "style-loader! css-loader"
             },
-            {
-                test:/\.html$/,
-                use: ['html-folder']
-            },{
-                test: /\.(jpg|png|svg)/,
-                use:   [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: '[name].[ext]',
-                            outputPath: 'static/',
-                            useRelativePath: true
-                        }
-                    },
-                    {
-                        loader: 'image-webpack-loader',
-                        options: {
-                            mozjpeg: {
-                                progressive: true,
-                                quality: 65
-                            },
-                            // optipng.enabled: false will disable optipng
-                            optipng: {
-                                enabled: false,
-                            },
-                            pngquant: {
-                                quality: '65-90',
-                                speed: 4
-                            },
-                            gifsicle: {
-                                interlaced: false,
-                            },
-                            // the webp option will enable WEBP
-                            webp: {
-                                quality: 75
-                            }
-                        }
-                    }
-                ]
-            }
-
+           
         ]
 
     }
